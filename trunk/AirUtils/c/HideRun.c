d758dc44f9c0894e1ef63730204a2f9d2dee7836
xtiqin <xtiqin@4b8b9bf5-dc4d-6cda-9559-2ebcebf18809>
2010-12-02 06:22:44 +0000

    更新 HideRun.c

--- trunk/AirUtils/c/HideRun.c
+++ trunk/AirUtils/c/HideRun.c
@@ -1,17 +1,29 @@
-//+---------------------------------------------------------------------------
-//
-//  HELLO_WIN.C - Windows GUI 'Hello World!' Example
-//
-//+---------------------------------------------------------------------------
+/*
+ ============================================================================
+ Name        : HideRun.c
+ Author      : xtiqin 
+ Website     : http://plter.com
+ Version     :
+ Copyright   : Your copyright notice
+ ============================================================================
+ */
 
+#include <stdio.h>
+#include <stdlib.h>
 #include <windows.h>
 
-int APIENTRY WinMain(
-				HINSTANCE hInstance,
-				HINSTANCE hPrevInstance,
-				LPSTR     lpCmdLine,
-				int       nCmdShow)
-{
-	WinExec(lpCmdLine,SW_HIDE);
-	return 1;
+int main(int argc,char** argv) {
+	int sw=0;
+	char cmdLine[512]="";
+	if(argc>1){
+		sw=strcmp(argv[1],"show")==0?SW_SHOW:SW_HIDE;
+		
+		int i=0;
+		for (i = 2; i < argc; ++i) {
+			strcat(cmdLine,argv[i]);
+			strcat(cmdLine," ");
+		}
+		WinExec(cmdLine,sw);
+	}
+	return EXIT_SUCCESS;
 }
