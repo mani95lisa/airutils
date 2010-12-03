e703c71487d892289fa3d3d78c98bcafbd81c031
xtiqin <xtiqin@4b8b9bf5-dc4d-6cda-9559-2ebcebf18809>
2010-12-03 05:53:22 +0000

    修改 HideRun.c

--- trunk/AirUtils/c/HideRun.c
+++ trunk/AirUtils/c/HideRun.c
@@ -8,22 +8,40 @@
  ============================================================================
  */
 
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <windows.h>
 
+
+/**
+ * 
+ * 命令行参数说明,sw delay cmd
+ * sw 是否显示窗体
+ * delay 延时
+ * cmd 要执行的命令
+ */
 int main(int argc,char** argv) {
 	int sw=0;
 	char cmdLine[512]="";
-	if(argc>1){
+	
+	if(argc>3){
 		sw=strcmp(argv[1],"show")==0?SW_SHOW:SW_HIDE;
+		int delay=atoi(argv[2]);
+		if(delay>0){
+			Sleep(delay);
+		}
 		
 		int i=0;
-		for (i = 2; i < argc; ++i) {
+		for (i = 3; i < argc; ++i) {
 			strcat(cmdLine,argv[i]);
 			strcat(cmdLine," ");
 		}
+		
 		WinExec(cmdLine,sw);
+	}else{
+		MessageBoxA(NULL,"Aurgument error","DelayHideRun",MB_OK);
 	}
 	return EXIT_SUCCESS;
 }
+
